d1b17a9064775dcb87a132d92735d2be94fb1f50
Ericprprpr <eric20000615@gmail.com>
2023-08-22 17:14:24 +0800

    example-10-func

--- 00-basic-usage/main.go
+++ 00-basic-usage/main.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+// func
+func add(a int, b int) int {
+	return a + b
+}
+
+func add2(a, b int) int {
+	return a + b
+}
+
+func exists(m map[string]string, k string) (v string, ok bool) {
+	v, ok = m[k]
+	return v, ok
+}
+
 func main() {
 	// var
 	//var a = "asdasdad"
@@ -169,4 +183,11 @@ func main() {
 		fmt.Println("key", k)
 	}
 
+	// func
+	res := add(1, 2)
+	fmt.Println(res)
+
+	v, ok := exists(map[string]string{"a": "A"}, "a")
+	fmt.Println(v, ok)
+	
 }
