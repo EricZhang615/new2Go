4a61765d23c63ed642c862ce9df4c5391f005613
Ericprprpr <eric20000615@gmail.com>
2023-08-23 17:57:38 +0800

    example-16-fmt

--- 00-basic-usage/main.go
+++ 00-basic-usage/main.go
@@ -36,6 +36,10 @@ type user struct {
 	password string
 }
 
+type point struct {
+	x, y int
+}
+
 func main() {
 	// var
 	//var a = "asdasdad"
@@ -264,6 +268,23 @@ func main() {
 	fmt.Println(len(aa))                                  // 5
 	bb := "你好"
 	fmt.Println(len(bb)) // 6
+
+	// fmt
+	ss := "hello"
+	n = 123
+	p := point{1, 2}
+	fmt.Println(s, n) // hello 123
+	fmt.Println(p)    // {1 2}
+
+	fmt.Printf("s=%v\n", ss) // s=hello
+	fmt.Printf("n=%v\n", n)  // n=123
+	fmt.Printf("p=%v\n", p)  // p={1 2}
+	fmt.Printf("p=%+v\n", p) // p={x:1 y:2}
+	fmt.Printf("p=%#v\n", p) // p=main.point{x:1, y:2}
+
+	f := 3.141592653
+	fmt.Println(f)          // 3.141592653
+	fmt.Printf("%.2f\n", f) // 3.14
 }
 
 // struct
