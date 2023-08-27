168ae849b59d5686b6a24769470a19584f781bb4
Ericprprpr <eric20000615@gmail.com>
2023-08-27 19:17:59 +0800

    01-example-proxy: auth connect

--- 01-example/proxy/main.go
+++ 01-example/proxy/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
 )
+
+const socks5Ver = 0x05
+const cmdBind = 0x01
+const aTypeIPV4 = 0x01
+const aTypeHOST = 0x03
+const aTypeIPV6 = 0x04
 
 func main() {
 	server, err := net.Listen("tcp", "172.30.192.1:1080")
@@ -24,14 +34,145 @@ func main() {
 func process(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
-	for true {
-		b, err := reader.ReadByte()
-		if err != nil {
-			break
+	/*
+		for true {
+			b, err := reader.ReadByte()
+			if err != nil {
+				break
+			}
+			_, err = conn.Write([]byte{b})
+			if err != nil {
+				break
+			}
 		}
-		_, err = conn.Write([]byte{b})
-		if err != nil {
-			break
-		}
+	*/
+	err := auth(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed: %v", conn.RemoteAddr(), err)
+		return
 	}
+	log.Println("auth success")
+
+	err = connect(reader, conn)
+	if err != nil {
+		log.Printf("client %v connect failed: %v", conn.RemoteAddr(), err)
+		return
+	}
+	log.Println("connect success")
+}
+
+func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+----------+----------+
+	// |VER | NMETHODS | METHODS  |
+	// +----+----------+----------+
+	// | 1  |    1     | 1 to 255 |
+	// +----+----------+----------+
+	// VER: 协议版本，socks5为0x05
+	// NMETHODS: 支持认证的方法数量
+	// METHODS: 对应NMETHODS，NMETHODS的值为多少，METHODS就有多少个字节。RFC预定义了一些值的含义，内容如下:
+	// X’00’ NO AUTHENTICATION REQUIRED
+	// X’02’ USERNAME/PASSWORD
+
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed: %w", err)
+	}
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver: %v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read method size failed: %w", err)
+	}
+	method := make([]byte, methodSize)
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed: %w", err)
+	}
+	log.Println("ver:", ver, "method:", method)
+
+	_, err = conn.Write([]byte{socks5Ver, 0x00})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+	return nil
+
+}
+
+func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+-----+-------+------+----------+----------+
+	// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER 版本号，socks5的值为0x05
+	// CMD 0x01表示CONNECT请求
+	// RSV 保留字段，值为0x00
+	// ATYP 目标地址类型，DST.ADDR的数据对应这个字段的类型。
+	//   0x01表示IPv4地址，DST.ADDR为4个字节
+	//   0x03表示域名，DST.ADDR是一个可变长度的域名
+	// DST.ADDR 一个可变长度的值
+	// DST.PORT 目标端口，固定2个字节
+
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("read header failed: %w", err)
+	}
+	ver, cmd, aType := buf[0], buf[1], buf[3]
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver: %v", ver)
+	}
+	if cmd != cmdBind {
+		return fmt.Errorf("not supported cmd: %v", cmd)
+	}
+	addr := ""
+	switch aType {
+	case aTypeIPV4:
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read addr failed: %w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case aTypeHOST:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read host size failed: %w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read host failed: %w", err)
+		}
+		addr = string(host)
+	case aTypeIPV6:
+		return errors.New("IPv6: not supported")
+	default:
+		return errors.New("invalid aType")
+	}
+	_, err = io.ReadFull(reader, buf[:2])
+	if err != nil {
+		return fmt.Errorf("read port failed: %w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+
+	log.Println("dial", addr, port)
+
+	// +----+-----+-------+------+----------+----------+
+	// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER socks版本，这里为0x05
+	// REP Relay field,内容取值如下 X’00’ succeeded
+	// RSV 保留字段
+	// ATYPE 地址类型
+	// BND.ADDR 服务绑定的地址
+	// BND.PORT 服务绑定的端口DST.PORT
+
+	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+	return nil
 }
