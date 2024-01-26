a98638d571595d84694fa4e0e38813c093c0c867
EricZhang615 <72425830+EricZhang615@users.noreply.github.com>
2024-01-26 17:17:04 +0800

    Create main.go

---
+++ Leetcode/860-lemonade-change/main.go
@@ -0,0 +1,24 @@
+package main
+
+func lemonadeChange(bills []int) bool {
+	five, ten := 0, 0
+	for _, bill := range bills {
+		if bill == 5 {
+			five++
+		} else if bill == 10 {
+			five--
+			ten++
+		} else {
+			if ten != 0 {
+				ten--
+				five--
+			} else {
+				five -= 3
+			}
+		}
+		if five < 0 || ten < 0 {
+			return false
+		}
+	}
+	return true
+}
