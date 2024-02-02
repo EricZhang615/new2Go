ee34d648db59245533936be902f7331220e846a2
EricZhang615 <72425830+EricZhang615@users.noreply.github.com>
2024-02-02 16:56:59 +0800

    Create main.go

---
+++ Leetcode/63-unique-paths-ii/main.go
@@ -0,0 +1,27 @@
+package main
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+	dp := make([][]int, m)
+
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+	for i := 0; i < n && obstacleGrid[0][i] == 0; i++ {
+		dp[0][i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
