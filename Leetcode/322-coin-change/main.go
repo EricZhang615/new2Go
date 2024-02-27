eba4ed8f241baa617bc072764500d9874a7202c8
EricZhang615 <72425830+EricZhang615@users.noreply.github.com>
2024-02-27 18:15:37 +0800

    Create main.go

---
+++ Leetcode/322-coin-change/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i < amount+1; i++ {
+		dp[i] = math.MaxInt
+	}
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			if dp[j-coins[i]] != math.MaxInt {
+				dp[j] = min(dp[j], dp[j-coins[i]]+1)
+			}
+		}
+	}
+	if dp[amount] == math.MaxInt {
+		return -1
+	}
+	return dp[amount]
+}
