d7bacf80f39578054ba7a86af580048038576441
EricZhang615 <72425830+EricZhang615@users.noreply.github.com>
2024-03-06 17:27:16 +0800

    Update main.go

--- Leetcode/704-binary-search/main.go
+++ Leetcode/704-binary-search/main.go
@@ -39,8 +39,46 @@ func search2(nums []int, target int) int {
 	return -1
 }
 
+func search3(nums []int, target int) int {
+	high := len(nums) - 1
+	low := 0
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] >= nums[low] && nums[high] >= nums[mid] {
+			if nums[mid] > target {
+				high = mid
+			} else {
+				low = mid + 1
+			}
+		} else {
+			if nums[mid] > nums[low] {
+				if nums[mid] > target && target >= nums[low] {
+					high = mid
+				} else {
+					low = mid + 1
+				}
+			} else {
+				if nums[mid] < target && target <= nums[high] {
+					low = mid + 1
+				} else {
+					high = mid
+				}
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
-	nums := []int{-1, 0, 3, 5, 9, 12}
-	target := 2
-	fmt.Println(search(nums, target))
+	//nums := []int{-1, 0, 3, 5, 9, 12}
+	//target := 2
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	target := 4
+	fmt.Println(search3(nums, target))
+	nums = []int{5, 5, 5, 1, 2, 3, 4}
+	target = 4
+	fmt.Println(search3(nums, target))
 }
